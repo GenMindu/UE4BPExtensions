26b07110cb1ac96c0678e02dcc1f6c52fd1b1ba5
Bruno Xavier Leite <bruno_xavier_@msn.com>
2015-02-16 11:58:15 -0200

    40 Nodes:  Branches and Math.
    First pack of nodes added.
    Focuses on extra C syntax ignored by Epic and recreates common branching
    nodes to avoid as much as possible the necessity of using the 'Branch'
    node, resulting on faster workflow and less redundant Blueprint
    sentences.

--- BxBLExtensions/Source/BxBLExtensions/Private/BxBLibrary.cpp
+++ BxBLExtensions/Source/BxBLExtensions/Private/BxBLibrary.cpp
@@ -1,21 +1,352 @@
 #include "BxBLExtensionsPrivatePCH.h"
 #include "BxBLibrary.h"
 
+
 ///////////////////////////////////////////////
 //     Blueprint Extension Library     //
 ///////////////////////////////////////////////
 
-UBxBLibrary::UBxBLibrary(const class FObjectInitializer& PCIP) : Super(PCIP)
+UBxBLibrary::UBxBLibrary(const class FObjectInitializer& PCIP) : Super(PCIP) {}
+//
+//
+/////////////////////////////////////////////////////////
+//     ::BRANCHES::                                      //
+/////////////////////////////////////////////////////////
+//
+//
+void UBxBLibrary::Branch_OEquals(UObject *Check, UObject *Against, TEnumAsByte<EN_Equals>&Branches)
 {
-	
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
 }
-/*
-FRotator UBxBLibrary::Conversions__VectorToRotator(const FVector& TheVector)
+//
+void UBxBLibrary::Branch_ONotEqual(UObject *Check, UObject *Against, TEnumAsByte<EN_NotEqual>&Branches)
 {
-	return TheVector.Rotation();
-}*/
-
-bool UBxBLibrary::Branch_Color(FLinearColor Check, FLinearColor Against, float fTolerance)
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	}
+	else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_STEquals(FString Check, FString Against, TEnumAsByte<EN_Equals>&Branches)
 {
-	return Check.Equals(Against, fTolerance);
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_TXEquals(FText Check, FText Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check.ToString() == Against.ToString()) {
+		Branches = EN_Equals::IfEquals;
+	} else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_INTEquals(int32 Check, int32 Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	} else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_INTNotEqual(int32 Check, int32 Against, TEnumAsByte<EN_NotEqual>&Branches)
+{
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	} else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+void UBxBLibrary::Branch_INTLessThan(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check < Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	} else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_INTGreaterThan(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check > Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	} else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_INTLessOREqual(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check <= Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	} else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_INTGreaterOREqual(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check >= Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	} else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_FEquals(float Check, float Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_FNotEqual(float Check, float Against, TEnumAsByte<EN_NotEqual>&Branches)
+{
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	}
+	else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+void UBxBLibrary::Branch_FLessThan(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check < Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	}
+	else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_FGreaterThan(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check > Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	}
+	else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_FLessOREqual(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check <= Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	}
+	else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+void UBxBLibrary::Branch_FGreaterOREqual(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches)
+{
+	if (Check >= Against) {
+		Branches = EN_IfThenElse::IfTrue;
+	}
+	else {
+		Branches = EN_IfThenElse::ElseDo;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_Enum(TEnumAsByte<EObjectTypeQuery>Check, TEnumAsByte<EObjectTypeQuery>Against, TEnumAsByte<EN_Match>&Branches)
+{
+	if (Check.GetValue() == Against.GetValue()) {
+		Branches = EN_Match::IfMatch;
+	}
+	else {
+		Branches = EN_Match::IfNot;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_Color(FLinearColor Check, FLinearColor Against, TEnumAsByte<EN_Match>&Branches, float Tolerance)
+{
+	if (Check.Equals(Against, Tolerance)) {
+		Branches = EN_Match::IfMatch;
+	} else {
+		Branches = EN_Match::IfNot;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_AEquals(AActor *Check, AActor *Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_ANotEqual(AActor *Check, AActor *Against, TEnumAsByte<EN_NotEqual>&Branches)
+{
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	}
+	else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_SCEquals(USceneComponent *Check, USceneComponent *Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_SCNotEqual(USceneComponent *Check, USceneComponent *Against, TEnumAsByte<EN_NotEqual>&Branches)
+{
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	}
+	else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+//
+void UBxBLibrary::Branch_PCEquals(UPrimitiveComponent *Check, UPrimitiveComponent *Against, TEnumAsByte<EN_Equals>&Branches)
+{
+	if (Check == Against) {
+		Branches = EN_Equals::IfEquals;
+	}
+	else {
+		Branches = EN_Equals::IfNot;
+	}
+}
+//
+void UBxBLibrary::Branch_PCNotEqual(UPrimitiveComponent *Check, UPrimitiveComponent *Against, TEnumAsByte<EN_NotEqual>&Branches)
+{
+	if (Check != Against) {
+		Branches = EN_NotEqual::IfNotEqual;
+	}
+	else {
+		Branches = EN_NotEqual::IfSo;
+	}
+}
+//
+//
+int32 UBxBLibrary::INTPosIncrement(int32 Value)
+{
+	return Value++;
+}
+//
+int32 UBxBLibrary::INTPreIncrement(int32 Value)
+{
+	return ++Value;
+}
+//
+int32 UBxBLibrary::INTPosDecrement(int32 Value)
+{
+	return Value--;
+}
+//
+int32 UBxBLibrary::INTPreDecrement(int32 Value)
+{
+	return --Value;
+}
+//
+int32 UBxBLibrary::INTIncrement(int32 Value, int32 i)
+{
+	return (Value += i);
+}
+//
+int32 UBxBLibrary::INTDecrement(int32 Value, int32 i)
+{
+	return (Value -= i);
+}
+//
+int32 UBxBLibrary::INTMultiply(int32 Value, int32 i)
+{
+	return (Value *= i);
+}
+//
+int32 UBxBLibrary::INTDivide(int32 Value, int32 i)
+{
+	return (Value /= i);
+}
+//
+int32 UBxBLibrary::INTModulo(int32 Value, int32 i)
+{
+	return (Value %= i);
+}
+//
+int32 UBxBLibrary::INTPercent(int32 Value, int32 i)
+{
+	return (Value = (Value*i) / 100);
+}
+//
+//
+float UBxBLibrary::FPosIncrement(float Value)
+{
+	return Value++;
+}
+//
+float UBxBLibrary::FPreIncrement(float Value)
+{
+	return ++Value;
+}
+//
+float UBxBLibrary::FPosDecrement(float Value)
+{
+	return Value--;
+}
+//
+float UBxBLibrary::FPreDecrement(float Value)
+{
+	return --Value;
+}
+//
+float UBxBLibrary::FIncrement(float Value, float f)
+{
+	return (Value += f);
+}
+//
+float UBxBLibrary::FDecrement(float Value, float f)
+{
+	return (Value -= f);
+}
+//
+float UBxBLibrary::FMultiply(float Value, float f)
+{
+	return (Value *= f);
+}
+//
+float UBxBLibrary::FDivide(float Value, float f)
+{
+	return (Value /= f);
+}
+//
+float UBxBLibrary::FPercent(float Value, float f)
+{
+	return (Value = (Value*f) / 100);
 }
--- BxBLExtensions/Source/BxBLExtensions/Private/BxBLibrary.h
+++ BxBLExtensions/Source/BxBLExtensions/Private/BxBLibrary.h
@@ -1,6 +1,43 @@
 #pragma once
 #include "BxBLibrary.generated.h"
 
+
+
+///////////////////////////////////////////////
+//     Custom Branching Types          //
+///////////////////////////////////////////////
+UENUM(BlueprintType)
+enum class EN_IsValid {
+	IsValid,
+	IsNot
+};
+
+UENUM(BlueprintType)
+enum class EN_Equals {
+	IfEquals,
+	IfNot
+};
+
+UENUM(BlueprintType)
+enum class EN_NotEqual {
+	IfNotEqual,
+	IfSo
+};
+
+UENUM(BlueprintType)
+enum class EN_Match {
+	IfMatch,
+	IfNot
+};
+
+UENUM(BlueprintType)
+enum class EN_IfThenElse {
+	IfTrue,
+	ElseDo
+};
+
+
+
 ///////////////////////////////////////////////
 //     Blueprint Extension Library     //
 ///////////////////////////////////////////////
@@ -11,9 +48,193 @@ class BXBLEXTENSIONS_API UBxBLibrary : public UBlueprintFunctionLibrary
 	GENERATED_UCLASS_BODY()
 	//
 	//
-	//** BRANCHES... Branching is love, branching is life. **//
+	/////////////////////////////////////////////////////////
+	//     ::BRANCHES::                                      //
+	/////////////////////////////////////////////////////////
 	//
-	/** IF Color == Input Color: **/
-	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Check: Color", Keywords = "check color"))
-	static bool Branch_Color(FLinearColor Check, FLinearColor Against, float fTolerance = 0.01f);
+	//
+	/** Branch:: Checks if object 'A' is equal to object 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Object A equals B?", Keywords = "bxbl object equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_OEquals(UObject *Check, UObject *Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if object 'A' is not equal to object 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Object A not equal to B?", Keywords = "bxbl object not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_ONotEqual(UObject *Check, UObject *Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	//
+	/** Branch:: Checks if string 'A' is equal to string 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "==", FriendlyName = "A equals B?", Keywords = "bxbl string equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_STEquals(FString Check, FString Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if text 'A' is equal to text 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "==", FriendlyName = "A equals B?", Keywords = "bxbl text equal", ExpandEnumAsExecs = "Branches"))
+		static void Branch_TXEquals(FText Check, FText Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	//
+	/** Branch:: Checks if int 'A' is equal to int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "==", FriendlyName = "A equals B?", Keywords = "bxbl int equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTEquals(int32 Check, int32 Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if int 'A' is not equal to int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "!=", FriendlyName = "A not equal to B?", Keywords = "bxbl int not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTNotEqual(int32 Check, int32 Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	/** Branch:: Checks if int 'A' is less than int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "<", FriendlyName = "A less than B?", Keywords = "bxbl int less", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTLessThan(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if int 'A' is greater than int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = ">", FriendlyName = "A greater than B?", Keywords = "bxbl int greater", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTGreaterThan(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if int 'A' is less or equal to int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "<=", FriendlyName = "A less or equal to B?", Keywords = "bxbl int less equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTLessOREqual(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if int 'A' is greater or equal to int 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = ">=", FriendlyName = "A greater pr equal to B?", Keywords = "bxbl int greater equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_INTGreaterOREqual(int32 Check, int32 Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	//
+	/** Branch:: Checks if float 'A' is equal to float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "==", FriendlyName = "A equals B?", Keywords = "bxbl float equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FEquals(float Check, float Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if float 'A' is not equal to float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "!=", FriendlyName = "A not equal to B?", Keywords = "bxbl float not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FNotEqual(float Check, float Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	/** Branch:: Checks if float 'A' is less than float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "<", FriendlyName = "A less than B?", Keywords = "bxbl float less", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FLessThan(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if float 'A' is greater than float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = ">", FriendlyName = "A greater than B?", Keywords = "bxbl float greater", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FGreaterThan(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if float 'A' is less or equal to float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "<=", FriendlyName = "A less or equal to B?", Keywords = "bxbl float less equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FLessOREqual(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	/** Branch:: Checks if float 'A' is greater or equal to float 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = ">=", FriendlyName = "A greater pr equal to B?", Keywords = "bxbl float greater equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_FGreaterOREqual(float Check, float Against, TEnumAsByte<EN_IfThenElse>&Branches);
+	//
+	//
+	/** Branch:: Checks if Enum 'A' matches Enum 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Does Enums Match?", Keywords = "bxbl enum match equals", ExpandEnumAsExecs = "Branches"))
+	static void Branch_Enum(TEnumAsByte<EObjectTypeQuery>Check, TEnumAsByte<EObjectTypeQuery>Against, TEnumAsByte<EN_Match>&Branches);
+	//
+	//
+	/** Branch:: Checks if Color 'A' matches Color 'B' by given Tolerance factor. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Does Colors Match?", Keywords = "bxbl color match equals", ExpandEnumAsExecs = "Branches"))
+	static void Branch_Color(FLinearColor Check, FLinearColor Against, TEnumAsByte<EN_Match>&Branches, float Tolerance = 0.01f);
+	//
+	//
+	/** Branch:: Checks if actor 'A' is equal to actor 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Actor A equals B?", Keywords = "bxbl actor equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_AEquals(AActor *Check, AActor *Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if actor 'A' is not equal to actor 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Actor A not equal to B?", Keywords = "bxbl actor not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_ANotEqual(AActor *Check, AActor *Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	//
+	/** Branch:: Checks if component 'A' is equal to component 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Component A equals B?", Keywords = "bxbl component equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_SCEquals(USceneComponent *Check, USceneComponent *Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if component 'A' is not equal to component 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Component A not equal to B?", Keywords = "bxbl component not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_SCNotEqual(USceneComponent *Check, USceneComponent *Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	//
+	/** Branch:: Checks if primitive 'A' is equal to primitive 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Primitive A equals B?", Keywords = "bxbl primitive equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_PCEquals(UPrimitiveComponent *Check, UPrimitiveComponent *Against, TEnumAsByte<EN_Equals>&Branches);
+	//
+	/** Branch:: Checks if primitive 'A' is not equal to primitive 'B'. */
+	UFUNCTION(BlueprintCallable, Category = "Blueprint Extensions|Branches", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "Primitive A not equal to B?", Keywords = "bxbl primitive not equal", ExpandEnumAsExecs = "Branches"))
+	static void Branch_PCNotEqual(UPrimitiveComponent *Check, UPrimitiveComponent *Against, TEnumAsByte<EN_NotEqual>&Branches);
+	//
+	//
+	/////////////////////////////////////////////////////////
+	//     ::ARITHMETICS::                                  //
+	/////////////////////////////////////////////////////////
+	//
+	//
+	/** Math:: (Pos)increments Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "++", FriendlyName = "(Pos)increment", Keywords = "bxbl int increment ++"))
+	static int32 INTPosIncrement(int32 Value);
+	//
+	/** Math:: (Pre)increments Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "++", FriendlyName = "(Pre)increment", Keywords = "bxbl int increment ++"))
+	static int32 INTPreIncrement(int32 Value);
+	//
+	/** Math:: (Pos)decrements Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "--", FriendlyName = "(Pos)decrement", Keywords = "bxbl int increment --"))
+	static int32 INTPosDecrement(int32 Value);
+	//
+	/** Math:: (Pre)decrements Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "--", FriendlyName = "(Pre)decrement", Keywords = "bxbl int increment --"))
+	static int32 INTPreDecrement(int32 Value);
+	//
+	/** Math:: (Plus) Value = Value+i. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "+=", FriendlyName = "Increment", Keywords = "bxbl int increment +="))
+	static int32 INTIncrement(int32 Value, int32 i);
+	/** Math:: (Minus) Value = Value-i. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "-=", FriendlyName = "Decrement", Keywords = "bxbl int decrement -="))
+	static int32 INTDecrement(int32 Value, int32 i);
+	//
+	/** Math:: (Multiply) Value = Value*i. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "*=", FriendlyName = "Multiply", Keywords = "bxbl int multiply *="))
+	static int32 INTMultiply(int32 Value, int32 i);
+	//
+	/** Math:: (Divide) Value = Value/i. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "/=", FriendlyName = "Divide", Keywords = "bxbl int divide /="))
+	static int32 INTDivide(int32 Value, int32 i);
+	//
+	/** Math:: (Mod) Value = Value%i. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "%=", FriendlyName = "Modulo", Keywords = "bxbl int modulo %="))
+		static int32 INTModulo(int32 Value, int32 i);
+	//
+	/** Math:: (Percent) Value = Value * i / 100. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "%%", FriendlyName = "Percent", Keywords = "bxbl int percent %%"))
+	static int32 INTPercent(int32 Value, int32 i);
+	//
+	//
+	/** Math:: (Pos)increments Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "++", FriendlyName = "(Pos)increment", Keywords = "bxbl float increment ++"))
+	static float FPosIncrement(float Value);
+	//
+	/** Math:: (Pre)increments Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "++", FriendlyName = "(Pre)increment", Keywords = "bxbl float increment ++"))
+	static float FPreIncrement(float Value);
+	//
+	/** Math:: (Pos)decrements Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "--", FriendlyName = "(Pos)decrement", Keywords = "bxbl float increment --"))
+	static float FPosDecrement(float Value);
+	//
+	/** Math:: (Pre)decrements Value. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "--", FriendlyName = "(Pre)decrement", Keywords = "bxbl float increment --"))
+	static float FPreDecrement(float Value);
+	//
+	/** Math:: (Plus) Value = Value + f. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "+=", FriendlyName = "Increment", Keywords = "bxbl float increment +="))
+	static float FIncrement(float Value, float f);
+	/** Math:: (Minus) Value = Value - f. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "-=", FriendlyName = "Decrement", Keywords = "bxbl float decrement -="))
+	static float FDecrement(float Value, float f);
+	//
+	/** Math:: (Multiply) Value = Value * f. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "*=", FriendlyName = "Multiply", Keywords = "bxbl float multiply *="))
+	static float FMultiply(float Value, float f);
+	//
+	/** Math:: (Divide) Value = Value / f. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "/=", FriendlyName = "Divide", Keywords = "bxbl float divide /="))
+	static float FDivide(float Value, float f);
+	//
+	/** Math:: (Percent) Value = Value * f / 100. */
+	UFUNCTION(BlueprintPure, Category = "Blueprint Extensions|Math", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", CompactNodeTitle = "%%", FriendlyName = "Percent", Keywords = "bxbl int percent %%"))
+	static float FPercent(float Value, float f);
 };
